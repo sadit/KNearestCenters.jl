d3b15c820acfa65234dc702eb0d5772372f4a5c5
Eric S. Tellez <donsadit@gmail.com>
2020-12-21 16:18:38 -0600

    docs

---
+++ docs/make.jl
@@ -0,0 +1,21 @@
+using KNearestCenters
+using Documenter
+
+makedocs(;
+    modules=[KNearestCenters],
+    authors="Eric S. Tellez",
+    repo="https://github.com/sadit/KNearestCenters.jl/blob/{commit}{path}#L{line}",
+    sitename="KNearestCenters.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://sadit.github.io/KNearestCenters.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/sadit/KNearestCenters.jl",
+)
---
+++ docs/src/index.md
@@ -0,0 +1,12 @@
+```@meta
+CurrentModule = KNearestCenters
+```
+
+# KNearestCenters
+
+```@index
+```
+
+```@autodocs
+Modules = [KNearestCenters]
+```
